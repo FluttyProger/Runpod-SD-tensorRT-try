16fc86d8e60b1d4bb3ed4fcffc2a3ccf4fc23d27
FluttyProger <ktaraso@yandex.ru>
2023-04-22 19:58:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,19 +12,18 @@ from diffusers.pipelines.stable_diffusion import StableDiffusionPipeline
 def init():
     global model
     
-    model_name = os.getenv("MODEL_NAME")
     scheduler = DDIMScheduler.from_pretrained("stabilityai/stable-diffusion-2-1",
                                                 subfolder="scheduler")
-
+    
     pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1",
                                                     custom_pipeline="stable_diffusion_tensorrt_txt2img",
                                                     revision='fp16',
                                                     torch_dtype=torch.float16,
                                                     scheduler=scheduler,)
-
+    
     # re-use cached folder to save ONNX models and TensorRT Engines
     pipe.set_cached_folder("stabilityai/stable-diffusion-2-1", revision='fp16',)
-
+    
     pipe = pipe.to("cuda")
 
 
