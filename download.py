398380aa1c7f2ff2fa8a6cbefa9b153e63ce780b
FluttyProger <ktaraso@yandex.ru>
2023-04-23 08:34:30 +0300

    Update download.py

--- download.py
+++ download.py
@@ -11,17 +11,20 @@ from diffusers.pipelines.stable_diffusion import StableDiffusionPipeline
 
 def download_model():
     model_name = os.getenv("MODEL_NAME")
-    scheduler = DDIMScheduler.from_pretrained("stabilityai/stable-diffusion-2-1",
+    model_rev = os.getenv("MODEL_REV")
+    scheduler = DDIMScheduler.from_pretrained(model_name,
                                                 subfolder="scheduler")
 
-    pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1",
+    model = StableDiffusionPipeline.from_pretrained(model_name,
                                                     custom_pipeline="stable_diffusion_tensorrt_txt2img",
-                                                    revision='fp16',
+                                                    revision=model_rev,
                                                     torch_dtype=torch.float16,
-                                                    scheduler=scheduler,)
+                                                    scheduler=scheduler)
 
     # re-use cached folder to save ONNX models and TensorRT Engines
-    pipe.set_cached_folder("stabilityai/stable-diffusion-2-1", revision='fp16',).to("cuda")
+    model.set_cached_folder(model_name, revision=model_rev)
+    
+    model = model.to("cuda")
     
 
 if __name__ == "__main__":
