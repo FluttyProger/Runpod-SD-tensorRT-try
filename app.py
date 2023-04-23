a46474007df23f140c5788bd3388568de8ca2a2f
FluttyProger <ktaraso@yandex.ru>
2023-04-23 14:48:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,31 +7,37 @@ from transformers import pipeline
 from diffusers import DDIMScheduler
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipeline
 
+os.environ["CUDA_MODULE_LOADING"] = "LAZY"
+
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    scheduler = DDIMScheduler.from_pretrained("stabilityai/stable-diffusion-2-1",
+    model_name = os.getenv("MODEL_NAME")
+    model_rev = os.getenv("MODEL_REV")
+    scheduler = DDIMScheduler.from_pretrained(model_name,
                                                 subfolder="scheduler")
     
-    model = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1",
-                                                    custom_pipeline="stable_diffusion_tensorrt_txt2img",
-                                                    revision='fp16',
+    model = StableDiffusionPipeline.from_pretrained(model_name,
+                                                    custom_pipeline="stable_diffusion_tensorrt_txt2img_edit",
+                                                    revision=model_rev,
                                                     torch_dtype=torch.float16,
-                                                    scheduler=scheduler,)
+                                                    scheduler=scheduler)
     
     # re-use cached folder to save ONNX models and TensorRT Engines
-    model.set_cached_folder("stabilityai/stable-diffusion-2-1", revision='fp16',)
+    model.set_cached_folder(model_name, revision=model_rev)
     
     model = model.to("cuda")
-
+    model.enable_model_cpu_offload()
+    model.enable_xformers_memory_efficient_attention()
 
 def inference(model_inputs:dict):
     global model
 
     prompt = model_inputs.get('prompt', None)
-    heigth = model_inputs.get('heigth', 768)
+    height = model_inputs.get('height', 768)
+    negative = model_inputs.get('negative_prompt', None)
+    num_images_per_prompt = model_inputs.get('num_images_per_prompt', 1)
     width = model_inputs.get('width', 768)
     steps = model_inputs.get('steps', 20)
     guidance_scale = model_inputs.get('guidance_scale', 9)
@@ -42,11 +48,12 @@ def inference(model_inputs:dict):
     generator = None
     if seed: generator = torch.Generator("cuda").manual_seed(seed)
     
-    with autocast("cuda"):
-        image = model(prompt, guidance_scale=guidance_scale, heigth=heigth, width=width, num_inference_steps=steps, generator=generator).images[0]
+    #with autocast("cuda"):
+    image = model(prompt, negative_prompt=negative, guidance_scale=guidance_scale, height=height, width=width, num_inference_steps=steps, generator=generator)
     
     buffered = BytesIO()
-    image.save(buffered, format="JPEG")
+    image.images[0].save(buffered, format="JPEG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
+    print(image)
     return {'image_base64': image_base64}
+
